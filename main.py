5f588c230cffd419cb0e62c70fa364229787592e
Omar <o.ouhbad@gmail.com>
2021-10-12 13:48:03 +0200

    + getLatest Reaperscans + favicon + reformat utils

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ from fastapi import FastAPI, HTTPException
 import json
 import importlib
 
+from starlette.responses import FileResponse
+from utils import utils
+
 app = FastAPI()
 
 @app.get('/')
 async def root():
+    return "Babel presentation"
+
+@app.get('/list')
+async def list():
     f = open('./sources.json',)
     res =  json.load(f)
     f.close()
     return res
 
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse('./assets/favicon.ico')
+
 @app.get('/{id}/')
 async def source(id):
-    source = getSource(id)
+    source = utils.getSource(id)
     if(source is None):
         raise HTTPException(status_code=404, detail="Source not found.")
     else:
@@ -21,21 +32,10 @@ async def source(id):
 
 @app.get('/{id}/latests/')
 async def latests(id):
-    source = getSource(id)
+    source = utils.getSource(id)
     if(source is None):
         raise HTTPException(status_code=404, detail="Source not found.")
     
     parser = importlib.import_module("sources.{}".format(source['id']))
 
     return parser.getLatests()
-
-
-def getSource(id):
-    f = open('./sources.json',)
-    res =  json.load(f)
-    f.close()
-
-    for source in res['sources']:
-        if source['id'] == id:
-            return source
-    return None
--- sources/RS.py
+++ sources/RS.py
@@ -1,2 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+from utils.utils import getSource
+
+RS = getSource('RS')
+
 def getLatests():
-    return "rs"
+    r = requests.get(RS['url_latests'])
+    html = BeautifulSoup(r.text, 'html.parser')
+
+    mangas = html.find_all('div', class_='bs')
+    
+    res = []
+    for item in mangas:
+        res.append({
+            "title": item.find('a')['title'],
+            "url": item.find('a')['href'],
+            "img": item.find('img')['src'],
+            "rating": item.find('div', class_='numscore').text
+        })
+
+    return res
